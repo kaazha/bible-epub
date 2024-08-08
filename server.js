4a671d37e7efcba2a00af721ab377ef3a48812fd
kaz <kazanno@outlook.com>
2024-08-08 06:55:48 -0700

    Added V2 files and moved previous files to V1 folder

--- server.js
+++ server.js
@@ -2,32 +2,61 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-let host = 'localhost';
+// e.g.:  node server 172.20.10.5:8000 
+
+let host = "localhost";
+let port = 8000;
+
 if (process.argv.length > 2) {
   host = process.argv[2];
-} 
+  let idx = host.indexOf(":");
+  if (idx > 0) {
+    port = host.substring(idx + 1);
+    host = host.substring(0, idx);
+  }
+}
+else {
+  const localIp = getLocalIP();
+  if(localIp) console.log(`My IPV Address: ${localIp}`);
+  host = localIp  || host;
+}
 
-//const host = "192.168.0.5"; //"localhost";
-const port = 8000;
+console.log(`starting server @${host}:${port}`);
 
 function getContentType(fileName) {
   const ext = path.extname(fileName);
   switch (ext) {
+    case ".html":
+      return "text/html";
     case ".xhtml":
     case ".opf":
       return "text/xhtml";
     case ".png":
       return "image/png";
+    case ".svg":
+      return "image/svg+xml";
     case ".woff":
     case ".woff2":
       return "font/woff";
     case ".css":
       return "text/css";
+    case ".js":
+      return "text/javascript";
     case ".json":
       return "application/json";
     default:
       console.error("mime type not found or mapped for: ", ext);
       return "text/plain";
+  }
+}
+
+function getLocalIP() {
+  const os = require("os");
+  const networkInterfaces = os.networkInterfaces();
+  for (const name of Object.keys(networkInterfaces)) {
+    for (const net of networkInterfaces[name]) {
+      if (net.family == 4 && !net.internal) return net.address;
+    }
   }
 }
 
@@ -39,7 +68,8 @@ const requestListener = async function (req, res) {
     return res.end("Method Not implemented.");
   }
 
-  const fileName = "." + req.url;
+  const fileName =
+    req.url === "" || req.url === "/" ? `./index.html` : `./${req.url}`;
   const contentType = getContentType(fileName);
   console.log(`${req.method} ${fileName} (${contentType})`);
   const stream = fs.createReadStream(fileName);
